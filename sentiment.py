542061c708b8fc6afb3f0a6fd80a92203969a604
los2119 Lily Sam <los2119@columbia.edu>
2021-06-29 21:25:46 +0000

    new commits

---
+++ sentiment.py
@@ -0,0 +1,12 @@
+import requests
+
+text = input()
+url = 'http://text-processing.com/api/sentiment/'
+myobj = {'text': text}
+#print(myobj['text'])
+
+response = requests.post(url, data = myobj)
+
+print(response.json())
+
+  
