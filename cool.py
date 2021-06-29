5178c070bc9efa4c7a4dc0bd16260407f7a80775
los2119 Lily Sam <los2119@columbia.edu>
2021-06-29 21:17:11 +0000

    new api code

---
+++ cool.py
@@ -0,0 +1,21 @@
+import requests
+import spotipy
+
+CLIENT_ID = '32289377e7b3432a9b90c1af5250e1ce'
+CLIENT_SECRET = '282d8ab89e854171a121b076fd2ade49'
+
+AUTH_URL = 'https://accounts.spotify.com/api/token'
+#POST
+auth_response = requests.post(AUTH_URL, {
+    'grant_type': 'client_credentials',
+    'client_id': CLIENT_ID,
+    'client_secret': CLIENT_SECRET,
+})
+
+print(auth_response.status_code)
+
+auth_response_data = auth_response.json()
+
+print(auth_response_data)
+
+access_token = auth_response_data['access_token']
