eb9f6936879fe62b9af6e85acb9437b0fb7a43d5
los2119 Lily Sam <los2119@columbia.edu>
2021-06-30 22:04:44 +0000

    einstein api

---
+++ einstein.py
@@ -0,0 +1,25 @@
+import sqlalchemy
+from sqlalchemy import create_engine
+from collections import defaultdict
+import pandas as pd
+import requests
+
+url = 'https://quotable.io/quotes?author=albert-einstein'
+
+details = requests.get(url)
+print(details.status_code)
+dict = details.json() #dictionary of einstein quotes
+quotes_info = dict['results'] # list of famous quotes with details
+my_dict = defaultdict(list)
+#print(quotes_info)
+for d in quotes_info:
+  for key, value in d.items():
+    my_dict[key].append(value)
+
+#converting dictionary into a pandas dataframe
+quotes_df = pd.DataFrame.from_dict(my_dict)
+quotes_df = quotes_df.drop(columns = ['tags'])
+#print(quotes_df)
+
+engine = create_engine('mysql://root:codio@localhost/einstein_quotes')
+quotes_df.to_sql('einstein_table', con = engine, if_exists = 'replace', index = False)
