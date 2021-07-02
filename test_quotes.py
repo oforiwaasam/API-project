64b143cecf1f15178dffb5d8684f75cd86ad58b0
los2119 Lily Sam <los2119@columbia.edu>
2021-07-02 13:08:16 +0000

    corrections to test

--- test_quotes.py
+++ test_quotes.py
@@ -1,9 +1,10 @@
 import unittest
-from einstein import get_json
+import pandas
+from quote_functions import get_json
 
 
 class TestFileName(unittest.TestCase):
-    def test_function1(self):
+    def test_get_json(self):
         url = 'https://api.quotable.io/random'
         json = get_json(url)
         self.assertNotEqual(json, None)
