65abd7d75253cd945ba3862dd9d78033b98f8439
los2119 Lily Sam <los2119@columbia.edu>
2021-07-13 18:44:50 +0000

    added audio file

--- demo.py
+++ demo.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm
 from flask_sqlalchemy import SQLAlchemy
+import time, random, threading
+from turbo_flask import Turbo
+from myaudio import printWAV
 
 
 # this gets the name of the file so Flask knows it's name
@@ -10,6 +13,9 @@ app.config['SECRET_KEY'] = '2e0d61f47091d568b375cd9f2838e9db'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 
+interval=10
+FILE_NAME = "The Hill We Climb.wav"
+turbo = Turbo(app)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -47,6 +53,49 @@ def register():
         flash(f'Account created for {form.username.data}!', 'success')
         return redirect(url_for('home'))  # if so - send to home page
     return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/captions")
+def captions():
+    TITLE = "The Hill We Climb"
+    return render_template('captions.html', songName=TITLE, file=FILE_NAME)
+
+
+@app.before_first_request
+def before_first_request():
+    #resetting time stamp file to 0
+    file = open("pos.txt","w") 
+    file.write(str(0))
+    file.close()
+
+    #starting thread that will time updates
+    threading.Thread(target=update_captions).start()
+
+
+@app.context_processor
+def inject_load():
+    # getting previous time stamp
+    file = open("pos.txt","r")
+    pos = int(file.read())
+    file.close()
+
+    # writing next time stamp
+    file = open("pos.txt","w")
+    file.write(str(pos+interval))
+    file.close()
+
+    #returning captions
+    return {'caption':printWAV(FILE_NAME, pos=pos, clip=interval)}
+
+
+def update_captions():
+    with app.app_context():
+        while True:
+            # timing thread waiting for the interval
+            time.sleep(interval)
+
+            # forcefully updating captionsPane with caption
+            turbo.push(turbo.replace(render_template('captionsPane.html'), 'load'))
 
 
 # this should always be at the end
